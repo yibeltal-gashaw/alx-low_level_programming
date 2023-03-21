0a8c8b81ca6929e4fff196381919233b0a74f8db
yibeltal-gashaw <yibeltalgashaw320@gmail.com>
2023-03-21 12:19:01 -0700

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+	/*
+	 * rint_alphabet - print all alphabet in lowercase
+	 */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
